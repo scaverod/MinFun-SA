faef5daf3c4f45d8b09317579288d4c7fd830984
unknown <david.delahoz.galiana@etsisi.upm.es>
2019-11-13 20:06:51 +0100

    mejorado termometro

--- termometro.py
+++ termometro.py
@@ -3,7 +3,7 @@ from math import e, cos, sin, tan, tanh, log
 
 
 # Temepratura estática en la que referencia a Kirkpatric 1985, no encuentro el paper
-def temperatura_kirkpatrick(iteracion, descanso, temperatura_actual, alpha=0.95, temperatura_inicial=1):
+def temperatura_kirkpatrick(iteracion, descanso, temperatura_actual, alpha, temperatura_inicial):
     if iteracion % descanso == 0:
         temperatura = alpha ** (iteracion / descanso) * temperatura_inicial
     else:
@@ -12,7 +12,7 @@ def temperatura_kirkpatrick(iteracion, descanso, temperatura_actual, alpha=0.95,
 
 
 def temperatura_lundy(temperatura, beta):
-    return temperatura / (1 + beta * temperatura)
+    return max (0.01, temperatura / (1 + beta * temperatura))
 
 
 def temperatura_hajek(iteracion, constante):
